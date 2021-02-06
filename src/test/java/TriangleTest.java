b08a64e767c07311fc1489624b069d4954c769dc
Łukasz Flis <luk8426@wp.pl>
2021-02-06 15:34:36 +0100

    JUnit Test

--- src/test/java/TriangleTest.java
+++ src/test/java/TriangleTest.java
@@ -2,31 +2,33 @@ import com.github.junittest.triangle.Triangle;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static com.github.junittest.triangle.Triangle.getType;
+
 public class TriangleTest {
 
     @Test
     public void analyzeGetType_throwsIllegalArgumentsException() {
         //given
-        Integer a = 0;
-        Integer b = 0;
-        Integer c = -1;
-        Triangle triangle = new Triangle(a, b, c);
+        int a = 0;
+        int b = 0;
+        int c = -1;
+        new Triangle(a, b, c);
         //when
 
         //then
-        Assertions.assertThrows(IllegalArgumentException.class, () -> Triangle.getType(a, b, c));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> getType(a, b, c));
     }
 
     @Test
     public void testGetType_equilateral() {
         //given
-        Triangle triangle = new Triangle(1, 1, 2);
-        Integer a = 1;
-        Integer b = 1;
-        Integer c = 1;
+        new Triangle(1, 1, 2);
+        int a = 1;
+        int b = 1;
+        int c = 1;
 
         //when
-        String result = triangle.getType(a, b, c);
+        String result = getType(a, b, c);
 
         //then
         Assertions.assertEquals("equilateral", result);
@@ -35,13 +37,13 @@ public class TriangleTest {
     @Test
     public void testGetType_scalene() {
         //given
-        Triangle triangle = new Triangle(1, 1, 2);
-        Integer a = 1;
-        Integer b = 2;
-        Integer c = 3;
+        new Triangle(1, 1, 2);
+        int a = 1;
+        int b = 2;
+        int c = 3;
 
         //when
-        String result = triangle.getType(a, b, c);
+        String result = getType(a, b, c);
 
         //then
         Assertions.assertEquals("scalene", result);
