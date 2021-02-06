1a00be6c7679e2cc1a71da0b10d1c8bae925f018
Łukasz Flis <luk8426@wp.pl>
2021-02-06 14:11:12 +0100

    Main class

---
+++ src/main/java/com/github/junittest/triangle/Triangle.java
@@ -0,0 +1,49 @@
+package com.github.junittest.triangle;
+
+public class Triangle {
+    int a;
+    int b;
+    int c;
+
+    /**
+     * Command line interface.
+     *
+     * @param args The command line parameters.
+     */
+    public static void main(String[] args) {
+        System.out.println(getType(2, 1, 1));
+    }
+
+    /**
+     * Initialize new object
+     *
+     * @param a first side of triangle
+     * @param b second side of triangle
+     * @param c third side of triangle
+     */
+    public Triangle(int a, int b, int c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    /**
+     * Returns the type of the triangle based on the given three side values.
+     * @param a first side of triangle.
+     * @param b second side of triangle.
+     * @param c third side of triangle.
+     * @return type of triangle.
+     */
+    public static String getType(int a, int b, int c) {
+        String type = "";
+        if (a == b && b == c) {
+            type = "equilateral";
+        } else if (a != b && b != c) {
+            type = "scalene";
+        } else if (a < 1 || b < 1 || a + b <= c || a + c <= b || b + c <= a) {
+            throw new IllegalArgumentException("Illegal arguments. Value must be greater then zero and sum of any two values must be greater than third value");
+        }
+
+        return type;
+    }
+}
